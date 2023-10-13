94413d16e87a3314a0e474efe11cb45253030c1c
James Newton <blackslate@lexogram.com>
2023-10-13 17:32:00 +0300

    Add "Recent" select element
    * Recent element hidden until the first class number is queried
    * Recent codes saved to localStorage
    * Enter key activates button regardless of which element has focus
    * Select updates value of text
    * CSS adjusted and refactored

--- openGmail.js
+++ openGmail.js
@@ -1,14 +1,30 @@
+/**
+ * openGmail.js
+ */
 
-const code = localStorage.getItem("zoom-code") || 12
+
+let codes = localStorage.getItem("zoom-codes") || "[]"
+codes = JSON.parse(codes)
+
 const url = "https://groups.google.com/a/digitalcareerinstitute.org/g/zoom-pm-"
 
 
+
+const recent = document.getElementById("recent")
+const select = document.getElementById("select")
+const button = document.getElementById("open-gmail")
 const text = document.getElementById("zoom-code")
 
-text.value = code
-text.focus()
-text.select()
-text.addEventListener("keydown", checkForEnter)
+
+
+select.addEventListener("change", selectRecent, false)
+button.addEventListener("click", openGmail, false)
+
+
+
+// Ensure that pressing the Enter key activates the button
+// regardless of which element has focus
+document.body.addEventListener("keydown", checkForEnter)
 
 function checkForEnter(event) {
   const key = event.key
@@ -20,11 +36,51 @@ function checkForEnter(event) {
 }
 
 
-const button = document.getElementById("open-gmail")
-button.addEventListener("click", openGmail, false)
+
+// Update the select element with the most recently used numbers
+function showRecentCodes() {
+  const innerHTML = codes.reduce(( html, code ) => {
+    return html + `<option value="${code}">${code}</option>`
+  }, "")
+
+  const action = innerHTML ? "remove" : "add"
+  recent.classList[action]("hidden")
+
+  select.innerHTML = innerHTML // may be empty
+  text.value = codes[0] || 12
+}
+
+showRecentCodes()
+
+
+
+// Allow user to start typing a number immediately
+text.focus()
+text.select()
+
+
+
+// Update the text input when a selection is made
+function selectRecent (event) {
+  text.value = event.target.value
+}
+
+
 
 function openGmail () {
   const code = text.value
+
+  // Remember the most recently used class code
+  const index = codes.indexOf(code)
+  if (index >= 0) {
+    codes.splice(index, 1)
+  }
+  codes.unshift(code)
+  codes.length = Math.min(5, codes.length)
+
+  showRecentCodes()
+  localStorage.setItem("zoom-codes", JSON.stringify(codes))
+
+  // Go to the appropiate Gmail account
   window.location.href = `${url}${code}`
-  localStorage.setItem("zoom-code", code)
 }
