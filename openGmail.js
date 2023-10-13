c4e05cb1a52dc2e93273596edff53862102f1f89
James Newton <blackslate@lexogram.com>
2023-10-13 15:20:42 +0300

    Basic functionality

---
+++ openGmail.js
@@ -0,0 +1,29 @@
+
+const code = localStorage.getItem("zoom-code") || 12
+const url = "https://groups.google.com/a/digitalcareerinstitute.org/g/zoom-pm-"
+
+
+const text = document.getElementById("zoom-code")
+
+text.value = code
+text.focus()
+text.addEventListener("keydown", checkForEnter)
+
+function checkForEnter(event) {
+  const key = event.key
+  if (key === "Enter") {
+    event.preventDefault((
+      openGmail()
+    ))
+  }
+}
+
+
+const button = document.getElementById("open-gmail")
+button.addEventListener("click", openGmail, false)
+
+function openGmail () {
+  const code = text.value
+  window.location.href = `${url}${code}`
+  localStorage.setItem("zoom-code", code)
+}
